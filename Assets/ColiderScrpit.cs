03d0be5e97391181a875e1888b0d2167f980f606
Te22e-Grufman-Axel <axel.grufman@elev.ga.ntig.se>
2024-10-18 13:21:18 +0200

    working on knight ai

--- Assets/ColiderScrpit.cs
+++ Assets/ColiderScrpit.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ColiderScrpit : MonoBehaviour
 {
-    
-    public  List<GameObject> CloseEnemys = new List<GameObject>();
+
+    public List<GameObject> CloseEnemys = new List<GameObject>();
+    public Knight_Ai knight_Ai;
     void Start()
     {
 
@@ -13,25 +15,25 @@ public class ColiderScrpit : MonoBehaviour
 
     void Update()
     {
-    // if (CloseEnemys.Count > 0)
-    // {
-    //   Debug.Log(CloseEnemys);
-    // }
+        if (CloseEnemys.Count > 0)
+        {
+          Debug.Log(CloseEnemys);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Ai_1" || other.gameObject.tag == "Ai_2" || other.gameObject.tag == "Ai_3")
         {
-          
-            CloseEnemys.Add(other.gameObject);
+            knight_Ai.CloseEnemys2.Add(other.gameObject);
         }
     }
     private void OnTriggerExit(Collider other)
     {
+
         if (other.gameObject.tag == "Ai_1" || other.gameObject.tag == "Ai_2" || other.gameObject.tag == "Ai_3")
         {
-            CloseEnemys.Remove(other.gameObject);
+            knight_Ai.CloseEnemys2.Remove(other.gameObject);
         }
     }
 }
--- Assets/Knight_Ai script.cs
+++ Assets/Knight_Ai script.cs
@@ -1,41 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Knight_Ai : MonoBehaviour
 {
   UnityEngine.AI.NavMeshAgent Knight;
   UnityEngine.AI.NavMeshPath navMeshPath;
-
   [SerializeField]
-  GameObject Ai_1_knight;
-  GameObject Colider;
+  GameObject RedFlag;
+  [SerializeField]
+  GameObject GreenFlag;
+  [SerializeField]
+  GameObject YellowFlag;
+
   public List<GameObject> CloseEnemys2 = new List<GameObject>();
-  // Start is called before the first frame update
+
   private void Awake()
   {
-    Colider = GameObject.Find("Colider");
     navMeshPath = new UnityEngine.AI.NavMeshPath();
     Knight = GetComponent<UnityEngine.AI.NavMeshAgent>();
   }
 
 
-  // Update is called once per frame
   void Update()
   {
-    CloseEnemys2.AddRange(Colider.GetComponent<ColiderScrpit>().CloseEnemys);
-
-    if(CloseEnemys2.Count > 0);
-
-
-
-
-
-    if (Knight.CalculatePath(Ai_1_knight.transform.position, navMeshPath) && navMeshPath.status == UnityEngine.AI.NavMeshPathStatus.PathComplete)
+    if (CloseEnemys2.Count > 0)
     {
-      //move to target
-      Knight.SetPath(navMeshPath);
+      if (Knight.CalculatePath(CloseEnemys2[0].transform.position, navMeshPath) && navMeshPath.status == UnityEngine.AI.NavMeshPathStatus.PathComplete)
+      {
+        Knight.SetPath(navMeshPath);
+        float lengt = Knight.remainingDistance;
+      }
+    }
+    else
+    {
+      Knight.CalculatePath(RedFlag.transform.position, navMeshPath);
+      float RedflagLenght = Knight.remainingDistance;
+      Knight.CalculatePath(RedFlag.transform.position, navMeshPath);
+      float GreenFlagLenght = Knight.remainingDistance;
+      Knight.CalculatePath(RedFlag.transform.position, navMeshPath);
+      float YellowFlagLenght = Knight.remainingDistance;
+
+     float closesfort = Math.Min(RedflagLenght, Math.Min(GreenFlagLenght, YellowFlagLenght));
+      
+
     }
   }
 }
