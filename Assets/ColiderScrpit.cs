1a39a4be391f22d7ffd35aec0c6b6c80c8008849
Te22e-Grufman-Axel <axel.grufman@elev.ga.ntig.se>
2024-11-01 15:54:11 +0100

    knight ai(exepct fightingn) done

--- Assets/ColiderScrpit.cs
+++ Assets/ColiderScrpit.cs
@@ -19,6 +19,8 @@ public class ColiderScrpit : MonoBehaviour
         {
         //   Debug.Log(CloseEnemys);
         }
+
+        
     }
 
     private void OnTriggerEnter(Collider other)
--- Assets/Knight_Ai script.cs
+++ Assets/Knight_Ai script.cs
@@ -42,35 +42,36 @@ public class Knight_Ai : MonoBehaviour
       float Reddistance = UnityEngine.Vector3.Distance(Knight.transform.position, RedFlag.transform.position);
       float Greendistance = UnityEngine.Vector3.Distance(Knight.transform.position, GreenFlag.transform.position);
       float Yellowdistance = UnityEngine.Vector3.Distance(Knight.transform.position, YellowFlag.transform.position);
-  
+
       float closesfort = Math.Min(Reddistance, Math.Min(Greendistance, Yellowdistance));
-Debug.Log("closesfort " + closesfort + " R " + Reddistance + " G " + Greendistance + " Y " + Yellowdistance);
+    
       if (closesfort == Reddistance)
       {
         Knight.CalculatePath(RedFlag.transform.position, navMeshPath);
         Knight.SetPath(navMeshPath);
-        Debug.Log("Red");
       }
       else if (closesfort == Greendistance)
       {
         Knight.CalculatePath(GreenFlag.transform.position, navMeshPath);
         Knight.SetPath(navMeshPath);
-        Debug.Log("green");
       }
       else if (closesfort == Yellowdistance)
       {
         Knight.CalculatePath(YellowFlag.transform.position, navMeshPath);
         Knight.SetPath(navMeshPath);
-        Debug.Log("yellow");
       }
-
-
-
-
-
-
-
-
     }
+
+
+
+    if(CloseEnemys2.Count > 0)
+    {
+        if(true)
+        {
+
+        }
+    }
+
+
   }
 }
